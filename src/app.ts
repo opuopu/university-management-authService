a6ad802b8b60833dfad9cbe82e81675fdaf656ba
opuopu <nurmdopu428@gmail.com>
2023-05-30 21:02:32 +0600

    husky,eslint,prettier

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-
 import cors from 'cors'
 import express, { Application } from 'express'
-export const app:Application = express()
+export const app: Application = express()
 export const port = 3000
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('database is perfectly working')
 })
-
