7e2f841767a5d17d83538e4619994938cd0b0a6f
opuopu <nurmdopu428@gmail.com>
2023-06-02 21:42:01 +0600

    logger setup

--- src/app/module/users/user.route.ts
+++ src/app/module/users/user.route.ts
@@ -1,4 +1,4 @@
 import express from 'express'
 import { createUsers } from './user.controller'
 export const router = express.Router()
-router.pos('/createuser', createUsers)
+router.post('/createuser', createUsers)
