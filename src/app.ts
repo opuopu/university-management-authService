216e0150f4ab47ba7a099f45bf0403c5fd45efd6
opuopu <nurmdopu428@gmail.com>
2023-06-07 21:25:35 +0600

    optimize application routes and controllers also optimize apiresponse json

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
 import cors from 'cors'
 import express, { Application } from 'express'
 import globalErrorHandler from './app/middlewares/GlobalErrorHandler'
-import { semesterRoutes } from './app/module/academic_semester/academic.semester.route'
-import { router as useRoutes } from './app/module/users/user.route'
+import routers from './app/routes'
 export const app: Application = express()
 export const port = 3000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/v1/users', useRoutes)
-app.use('/api/v1/academic-semesters', semesterRoutes)
+app.use('/api/v1', routers)
 
 // global error handler
 
